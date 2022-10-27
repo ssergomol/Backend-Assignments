afb5f4407f2fa662af94171aeb44285c331ec8d2
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-10-27 23:47:50 +0300

    add server start cmds

--- L0/l0-solution/cmd/main.go
+++ L0/l0-solution/cmd/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"backend-assignments/l0/pkg/apiserver"
 	"backend-assignments/l0/pkg/streaming"
+	"log"
 )
 
 func main() {
+	// Retrieve data from streaming server
 	streaming.PublishDataToChannel()
 	streaming.GetDataFromChannel()
+
+	config := apiserver.NewConfig()
+	server := apiserver.NewServer(config)
+
+	if err := server.Start(); err != nil {
+		log.Fatal(err)
+	}
+
 }

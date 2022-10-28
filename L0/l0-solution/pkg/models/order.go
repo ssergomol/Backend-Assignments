ebd5f9e1fdcd4ba240612c7cada61428c473a5ee
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-10-29 01:14:40 +0300

    add sql models

---
+++ L0/l0-solution/pkg/models/order.go
@@ -0,0 +1,15 @@
+package models
+
+type Order struct {
+	OrderUID          string `json:"order_uid"`
+	TrackNumber       string `json:"track_number"`
+	Entry             string `json:"entry"`
+	Locale            string `json:"locale"`
+	InternalSignature string `json:"internal_signature"`
+	CustomerID        string `json:"customer_id"`
+	DeliveryService   string `json:"delivery_service"`
+	ShardKey          string `json:"shardkey"`
+	SmID              int    `json:"sm_id"`
+	DateCreated       string `json:"date_created"`
+	OofShard          string `json:"oof_shard"`
+}

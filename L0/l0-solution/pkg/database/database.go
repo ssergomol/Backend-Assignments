07a16de2d29a584d9a39140f4f08bb77196e4410
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-10-29 02:52:41 +0300

    add data access funcs

--- L0/l0-solution/pkg/database/database.go
+++ L0/l0-solution/pkg/database/database.go
@@ -7,8 +7,12 @@ import (
 )
 
 type Store struct {
-	db     *sql.DB
-	Config *ConfigDB
+	db           *sql.DB
+	Config       *ConfigDB
+	orderRepo    *OrderRepo
+	itemRepo     *ItemRepo
+	deliveryRepo *DeliveryRepo
+	paymentRepo  *PaymentRepo
 }
 
 func NewDB(config *ConfigDB) *Store {
@@ -36,4 +40,52 @@ func (s *Store) Connect() error {
 
 func (s *Store) Disconnect() error {
 	return s.db.Close()
+}
+
+func (s *Store) Order() *OrderRepo {
+	if s.orderRepo != nil {
+		return s.orderRepo
+	}
+
+	s.orderRepo = &OrderRepo{
+		store: s,
+	}
+
+	return s.orderRepo
+}
+
+func (s *Store) Item() *ItemRepo {
+	if s.itemRepo != nil {
+		return s.itemRepo
+	}
+
+	s.itemRepo = &ItemRepo{
+		store: s,
+	}
+
+	return s.itemRepo
+}
+
+func (s *Store) Delivery() *DeliveryRepo {
+	if s.itemRepo != nil {
+		return s.deliveryRepo
+	}
+
+	s.deliveryRepo = &DeliveryRepo{
+		store: s,
+	}
+
+	return s.deliveryRepo
+}
+
+func (s *Store) Payment() *PaymentRepo {
+	if s.paymentRepo != nil {
+		return s.paymentRepo
+	}
+
+	s.paymentRepo = &PaymentRepo{
+		store: s,
+	}
+
+	return s.paymentRepo
 }

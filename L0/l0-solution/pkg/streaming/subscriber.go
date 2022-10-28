527b3f7e5a25fda5062dd9987a5bb0a4c796a9ce
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-10-29 01:14:15 +0300

    add data marshaling to structs

--- L0/l0-solution/pkg/streaming/subscriber.go
+++ L0/l0-solution/pkg/streaming/subscriber.go
@@ -1,11 +1,20 @@
 package streaming
 
 import (
+	"backend-assignments/l0/pkg/models"
+	"encoding/json"
 	"fmt"
 	"sync"
 
 	"github.com/nats-io/stan.go"
 )
+
+type JSONstructure struct {
+	models.Order
+	Delivery models.Delivery `json:"delivery"`
+	Payment  models.Payment  `json:"payment"`
+	Items    []models.Item   `json:"items"`
+}
 
 func GetDataFromChannel() {
 	const (
@@ -18,12 +27,28 @@ func GetDataFromChannel() {
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
+	channel := make(chan []byte)
 
 	sub, _ := conn.Subscribe("channel", func(m *stan.Msg) {
 		fmt.Printf("Received a message: %v\n", string(m.Data))
+		channel <- m.Data
 		wg.Done()
 	}, stan.StartWithLastReceived())
+	var data []byte
+	data = <-channel
+	UnmarshalJSON(data)
 
 	wg.Wait()
+
 	sub.Unsubscribe()
+}
+
+func UnmarshalJSON(rawData []byte) {
+	data := JSONstructure{}
+	json.Unmarshal(rawData, &data)
+	data.Delivery.OrderUID = data.OrderUID
+	data.Payment.OrderUID = data.OrderUID
+	for i := range data.Items {
+		data.Items[i].OrderUID = data.OrderUID
+	}
 }
